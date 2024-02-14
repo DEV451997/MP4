99ac1698fb0c14b7086696de897683542507f323
Arron Evans <aredev45@gmail.com>
2024-02-14 13:22:36 +0000

    Created edit blog template, views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import blog
+from . import views
 
 urlpatterns = [
     path('', blog, name='blog'),
+    path('edit/<int:post_id>/', views.edit_blog, name='edit_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,3 +9,16 @@ def blog(request):
         'categories': categories,
     }
     return render(request, 'blog/blog.html', context)
+
+def edit_blog(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:blog', post_id=post.id)  # Redirect to detail view
+    else:
+        form = BlogPostForm(instance=post)
+    
+    return render(request, 'edit_blog.html', {'form': form})
