2633a78159edf7c07f6bc2b9f18353470d4bb99b
Arron Evans <aredev45@gmail.com>
2024-04-01 13:58:13 +0000

    Added price form validation to prevent negative or zero sum pricing

--- products/forms.py
+++ products/forms.py
@@ -49,3 +49,23 @@ class ProductForm(forms.ModelForm):
             raise forms.ValidationError('Rating must be between 1 and 5.')
 
         return rating
+
+    def clean_price(self):
+        """
+        Custom cleaning method for the 'price' field.
+
+        This method ensures that the price is greater than or equal to 0.01.
+
+        Returns:
+            float: The cleaned price value.
+
+        Raises:
+            forms.ValidationError:
+            If the price is less than 0.01.
+        """
+        price = self.cleaned_data.get('price')
+
+        if price is not None and price <= 0.00:
+            raise forms.ValidationError('Price must be at least 0.01.')
+
+        return price
