39697f25cead7d2840d5a9013f7e44c9e9644e3e
Arron Evans <aredev45@gmail.com>
2024-04-05 14:53:01 +0000

    sending real emails

--- mt_olympus/settings.py
+++ mt_olympus/settings.py
@@ -232,5 +232,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
-NOTIFY_EMAIL = os.environ.get('NOTIFY_EMAIL', 'notification@example.com')
